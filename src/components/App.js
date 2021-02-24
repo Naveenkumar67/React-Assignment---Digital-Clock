62cb943a4fb1bee2dd719881af6ad88c40655df2
Naveenkumar67 <naveenvempati4@gmail.com>
2021-02-24 15:31:32 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+   constructor(props){
+       super(props)
+       this.state={
+        time:Date.now()
+       }
+   }
+   componentDidMount(){
+       this.state.time;
+   }
     render() {
 
         return(
-            <>
-               
-            </>
+            
+              <div className="Clock">
+                <h3 id="time">{this.state.time.toLocaleString()}</h3>  
+                </div> 
+          
         )
     }
 }
