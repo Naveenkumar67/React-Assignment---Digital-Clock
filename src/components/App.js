921c637c13ac8ac03978afd28f0578103637e678
Naveenkumar67 <naveenvempati4@gmail.com>
2021-02-26 20:07:11 +0530

    2nd

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,25 @@ class App extends Component {
    constructor(props){
        super(props)
        this.state={
-        time:Date.now()
+        time:new Date()
        }
    }
    
    componentDidMount(){
        this.update=setInterval(()=>{
-           this.setState({time:Date.now()})
+           this.setState({time:new Date().toLocaleTimeString})
        },1000)
    }
    componentWillUnmount(){
        clearInterval(this.update);
    }
-   
+
     render() {
 
         return(
             <div id="root">
               <div className="Clock">
-                <h3 id="time">{this.state.time.toLocaleString()}</h3>  
+                <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>  
                 </div> 
                 </div>   
           
